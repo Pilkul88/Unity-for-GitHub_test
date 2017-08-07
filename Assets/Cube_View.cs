bf97af9964010eabfefabb6a8f63afb5ac8c8451
pilkul <pilkul.energydrink@gmail.com>
2017-08-07 22:59:13 +0900

    最初のコミット

---
+++ Assets/Cube_View.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_View : MonoBehaviour {
+
+	public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 vec = transform.position;
+		if (vec.x < 0 || vec.x > 5) {
+			speed *= -1;
+		}
+		transform.position += new Vector3 (speed, 0, 0);
+	}
+}
